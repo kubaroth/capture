5ed12baa6f883faa97b2a045c0e5888794d2a4cb
kubaroth <kubaroth@gmail.com>
2020-08-26 22:15:37 -0700

    test generating 7x7 png image

--- tests/image.cpp
+++ tests/image.cpp
@@ -3,21 +3,43 @@
 
 #include "../Catch2/single_include/catch2/catch.hpp"
 #include "../src/rgb.h"
+#include "../src/png.h"
 
 using namespace vpl;
 
-TEST_CASE( "5x5" ) {
+TEST_CASE( "save 7x7" ) {
     using namespace std;
-    std::vector<RGB> rgbs;
-    PageInfo info;
-    info.width = 3;
-    info.height = 3;
-    for (int y=0; y < info.height; y++){
-        for (int x=0; x < info.width; x++){
-            rgbs.emplace_back(RGB(x,y,0,x,y, rgbs.size()) );
-        }
-    }
-    // for( auto px : rgbs) cout << px << endl; // output each pixel value
 
+    vector<int> image_values{
+        1,1,1,1,1,1,1,
+        1,0,0,0,0,0,1,
+        1,0,0,0,0,0,1,
+        1,0,0,0,0,0,1,
+        1,0,0,0,0,0,1,
+        1,0,0,0,0,0,1,
+        1,1,1,1,1,1,1
+    };
+
+    vector<RGB> rgbs;
+    transform(image_values.begin(), image_values.end(), back_inserter(rgbs), [index=0](int i) mutable
+    {
+        int y = index / 7;
+        int x = index % 7;
+        if (i == 1){
+            return RGB(255, 255, 255, x, y, index);
+        }
+        else{
+            return RGB(240, 240, 240, x, y, index);
+        }
+        index++;
+    });
+    for(auto i : rgbs) cout << i << " ";
+    
+
+    PageInfo info;
+    info.width = 7;
+    info.height = 7;
+    info.filename = "gray_7x7.png";
+    savepng(rgbs, info);
 }
 
