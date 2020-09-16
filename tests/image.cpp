5eeb0088f51cb4815055ca9210f31eee50e77cd7
kubaroth <kubaroth@gmail.com>
2020-09-16 11:39:46 -0700

    tests image - wip

--- tests/image.cpp
+++ tests/image.cpp
@@ -7,7 +7,7 @@
 
 using namespace vpl;
 
-TEST_CASE( "save 7x7" ) {
+TEST_CASE( "7x7 image" ) {
     using namespace std;
 
     vector<int> image_values{
@@ -19,27 +19,30 @@ TEST_CASE( "save 7x7" ) {
         1,0,0,0,0,0,1,
         1,1,1,1,1,1,1
     };
-
-    vector<RGB> rgbs;
-    transform(image_values.begin(), image_values.end(), back_inserter(rgbs), [index=0](int i) mutable
-    {
-        int y = index / 7;
-        int x = index % 7;
-        if (i == 1){
-            return RGB(255, 255, 255, x, y, index);
-        }
-        else{
-            return RGB(240, 240, 240, x, y, index);
-        }
-        index++;
-    });
-    for(auto i : rgbs) cout << i << " ";
-    
-
     PageInfo info;
     info.width = 7;
     info.height = 7;
     info.filename = "gray_7x7.png";
-    savepng(rgbs, info);
+
+    SECTION ( "save image"){
+        vector<RGB> rgbs;
+        transform(image_values.begin(), image_values.end(), back_inserter(rgbs), [index=0](int i) mutable {
+            int y = index / 7;
+            int x = index % 7;
+            if (i == 1){
+                return RGB(255, 255, 255, x, y, index);
+            }
+            else{
+                return RGB(240, 240, 240, x, y, index);
+            }
+            index++;
+         });
+         savepng(rgbs, info);
+    }
+    SECTION ("load image"){
+        REQUIRE (info.filename.compare("gray_7x7.png") == 0);
+    }
+
+
 }
 
